fdba44d5a9ea5e8751ee12953ad8c4c0e3794188
Patrick Krämer <patagonahn@gmail.com>
2020-07-14 02:13:39 +0200

    fix difficulty scraping

--- src/BeatSaverMatcher.Common/Db/SongDifficulties.cs
+++ src/BeatSaverMatcher.Common/Db/SongDifficulties.cs
@@ -5,10 +5,10 @@ namespace BeatSaverMatcher.Common.Models
     [Flags]
     public enum SongDifficulties
     {
-        Easy,
-        Normal,
-        Hard,
-        Expert,
-        ExpertPlus
+        Easy = 1 << 0,
+        Normal = 1 << 1,
+        Hard = 1 << 2,
+        Expert = 1 << 3,
+        ExpertPlus = 1 << 4
     }
 }
